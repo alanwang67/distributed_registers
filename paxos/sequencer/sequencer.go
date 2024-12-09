6ba45bd054016b809799af9a52c4dfefea8666de
davidh2139 <davidh2139@gmail.com>
2024-12-09 12:07:00 -0500

    paxos eval
    just ran the eval based on what professor shin said to do
    
    "It can be simply proposers trying to write the same value over and over again and proposer or users reading the quorum value (using the first phase). Different from the regular first phase, the quorum read should check if the majority holds the same value with the same proposal number before returning a successful read; otherwise read may need to repeat the commit phase similar to ABD to ensure the non-quorum value commits."

--- paxos/sequencer/sequencer.go
+++ paxos/sequencer/sequencer.go
@@ -1,10 +1,29 @@
 package sequencer
 
 import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"sync"
+
 	"github.com/alanwang67/distributed_registers/paxos/protocol"
+	"github.com/charmbracelet/log"
 )
 
-// New creates and initializes a new Server instance with the given ID, self connection, and peer connections.
+type Sequencer struct {
+	Count uint64
+	Self  *protocol.Connection
+	mu    sync.Mutex
+}
+
+type ReqProposalNum struct {
+}
+
+type ReplyProposalNum struct {
+	Count uint64
+}
+
+// New creates and initializes a new Sequencer instance with the given self connection.
 func New(self *protocol.Connection) *Sequencer {
 	s := &Sequencer{
 		Self:  self,
@@ -13,10 +32,32 @@ func New(self *protocol.Connection) *Sequencer {
 	return s
 }
 
-func (s *Sequencer) GetProposalNumber(_, reply *ReplyProposalNum) error {
+// GetProposalNumber increments and returns the current proposal count.
+func (s *Sequencer) GetProposalNumber(_ *ReqProposalNum, reply *ReplyProposalNum) error {
 	s.mu.Lock()
 	reply.Count = s.Count
-	s.Count += 1
+	s.Count++
 	s.mu.Unlock()
+	log.Debugf("Sequencer returned proposal number %d", reply.Count)
 	return nil
+}
+
+// Start begins listening for RPC requests on the sequencer's configured address.
+func (s *Sequencer) Start() error {
+	log.Debugf("starting sequencer")
+
+	l, err := net.Listen(s.Self.Network, s.Self.Address)
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+	defer l.Close()
+	log.Debugf("sequencer listening on %s", s.Self.Address)
+
+	rpc.Register(s)
+
+	for {
+		rpc.Accept(l)
+		// Other code could go here, if needed
+	}
 }
